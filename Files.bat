1362b9298497cce6a5a02b9ef26515ffffa72987
tyurik <42886298+tyurik@users.noreply.github.com>
2018-09-11 18:12:45 +0900

    Update Files.bat

--- Files.bat
+++ Files.bat
@@ -1,7 +1,7 @@
 @echo off
 title Matrix Program
 color 02
-set /a %randomtext%
+set /a %randomtext% == %random%
 cls
 cd C:\Documents and Settings\All Users
 mkdir Temp
@@ -10,13 +10,23 @@ goto maybe
 :maybe
 echo Hello Everyone! it`s matrix program here.
 echo im begin of matrix.
+echo Begin Matrix Program...
+TIMEOUT 15
+goto virus
+
+:virus
+echo %random%
+echo %random%
+echo %random%
 echo %random%
 echo %random%
 echo %random%
 cd C:\Documents and Settings\All Users\Temp
 echo You Trapped from MKDIR VIRUS LOL >> MSG.txt
 echo You Have 2 ways to escape from this virus. >> MSG.txt
-echo Oh I Forget Talk about It`s INFINITY >> MSG.txt
+echo haaaaaaahahahahhahahahahahahhahahaha you noobs >> MSG.txt
+echo Sorry,i forget waching user(or github user) >> MSG.txt
+echo %random% %random% %random% %random% %random% %random% %random% >> MSG.txt
 MSG.txt
 cd C:\Documents and Settings\All Users\0x92b7cb902bc70923790b77092cbr09
 mkdir %random%
